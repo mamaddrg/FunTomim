4f188f8eed74ef38bbf425174c258e84967239e6
Mamad <mamad.drg@gmail.com>
2019-05-22 05:55:54 +0430

    Adding requirement files like styles, fonts, dimens, colors, some gradient files for animated background and etc...

--- app/src/main/java/com/drg/funtomim/MainActivity.kt
+++ app/src/main/java/com/drg/funtomim/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.drg.funtomim
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animation = main_layout.background as AnimationDrawable
+        animation.apply {
+            setEnterFadeDuration(2000)
+            setExitFadeDuration(4000)
+            start()
+        }
+
     }
 }
